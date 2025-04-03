27a57e53a25d7aec117a719cd938663141b58e3c
alexa <alex@targoninc.com>
2025-04-03 18:49:35 +0200

    bump version automatically?

--- .scripts/copy-version.js
+++ .scripts/copy-version.js
@@ -3,6 +3,7 @@ import fs from "fs";
 const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
 const version = packageJson.version;
 const versionFilePath = "./src/assets/version.txt";
+console.log("Version: " + version);
 fs.writeFileSync(versionFilePath, version);
 
 console.log("Version copied to " + versionFilePath);
