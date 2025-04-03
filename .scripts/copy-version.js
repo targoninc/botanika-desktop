515b22cfcded8b3213cfd4936ad310147cac543a
alexa <alex@targoninc.com>
2025-04-03 18:48:06 +0200

    version txt

--- .scripts/copy-version.js
+++ .scripts/copy-version.js
@@ -1,9 +1,8 @@
-// copy version from package.json to src/ui/js/Classes/Helpers/version.txt
 import fs from "fs";
 
 const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
 const version = packageJson.version;
-const versionFilePath = "./src/ui/js/Classes/Helpers/version.txt";
+const versionFilePath = "./src/assets/version.txt";
 fs.writeFileSync(versionFilePath, version);
 
 console.log("Version copied to " + versionFilePath);
