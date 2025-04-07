43e417448e7e3ef79824ab38cef7a75d30e81f4f
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2025-04-07 16:38:38 +0200

    extract shortcuts

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,8 +1,6 @@
 import {LayoutTemplates} from "./templates/layout.templates";
-import {closeModal} from "./classes/ui";
-import {activePage, initializeStore, shortCutConfig, target} from "./classes/store";
-import {pages} from "./enums/pages";
-import {shortCutActions} from "./classes/shortCutActions";
+import {activePage, initializeStore} from "./classes/store";
+import {addShortCutListener} from "./classes/shortcuts";
 
 initializeStore();
 
@@ -10,26 +8,4 @@ const content = document.getElementById('content');
 const app = LayoutTemplates.app(activePage);
 content.appendChild(app);
 
-const blockShortcuts = ["INPUT", "TEXTAREA", "SELECT"];
-document.addEventListener("keydown", (e) => {
-    const shortcutConfig = shortCutConfig.value;
-    for (const [action, func] of Object.entries(shortCutActions)) {
-        if (e.ctrlKey && e.key === shortcutConfig[action]) {
-            e.preventDefault();
-            func();
-        }
-    }
-
-    if (blockShortcuts.includes(target(e).tagName)) {
-        return;
-    }
-
-    const isNumber = e.key.match(/^[0-9]+$/);
-    if (isNumber) {
-        activePage.value = pages.find((p, i) => p.hotkey === e.key)?.id ?? "chat";
-    }
-
-    if (e.key === "Escape") {
-        closeModal();
-    }
-});
+addShortCutListener();
