452ddbdb9b1c398cae79fd6547cc2c8b85aab254
alexa <alex@targoninc.com>
2025-04-16 23:07:38 +0200

    add new openai models

--- src/api/ai/llms/providers/openai.ts
+++ src/api/ai/llms/providers/openai.ts
@@ -3,9 +3,9 @@ import {ModelDefinition} from "../../../../models/ModelDefinition";
 export function getOpenaiModels(): ModelDefinition[] {
     return [
         {
-            id: "o3-mini-2025-01-31",
-            displayName: "o3-mini-2025-01-31",
-            supportsTools: false
+            id: "o4-mini",
+            displayName: "o4-mini",
+            supportsTools: true
         },
         {
             id: "o3-mini",
@@ -13,33 +13,8 @@ export function getOpenaiModels(): ModelDefinition[] {
             supportsTools: false
         },
         {
-            id: "o1-preview-2024-09-12",
-            displayName: "o1-preview-2024-09-12",
-            supportsTools: false
-        },
-        {
-            id: "gpt-4o-mini-search-preview",
-            displayName: "GPT 4o-mini-search-preview",
-            supportsTools: true
-        },
-        {
-            id: "gpt-4o-mini-search-preview-2025-03-11",
-            displayName: "GPT 4o-mini-search-preview-2025-03-11",
-            supportsTools: true
-        },
-        {
-            id: "gpt-4o-2024-11-20",
-            displayName: "GPT 4o-2024-11-20",
-            supportsTools: true
-        },
-        {
-            id: "gpt-3.5-turbo-0125",
-            displayName: "GPT 3.5-turbo-0125",
-            supportsTools: false
-        },
-        {
-            id: "gpt-4o-2024-05-13",
-            displayName: "GPT 4o-2024-05-13",
+            id: "o3",
+            displayName: "o3",
             supportsTools: true
         },
         {
@@ -48,33 +23,18 @@ export function getOpenaiModels(): ModelDefinition[] {
             supportsTools: false
         },
         {
-            id: "gpt-4o-search-preview",
-            displayName: "GPT 4o-search-preview",
-            supportsTools: true
-        },
-        {
-            id: "gpt-4o-search-preview-2025-03-11",
-            displayName: "GPT 4o-search-preview-2025-03-11",
-            supportsTools: true
-        },
-        {
-            id: "o1-2024-12-17",
-            displayName: "o1-2024-12-17",
-            supportsTools: false
-        },
-        {
             id: "o1",
             displayName: "o1",
             supportsTools: false
         },
         {
-            id: "o1-pro",
-            displayName: "o1-pro",
+            id: "o1-mini",
+            displayName: "o1-mini",
             supportsTools: false
         },
         {
-            id: "o1-pro-2025-03-19",
-            displayName: "o1-pro-2025-03-19",
+            id: "o1-pro",
+            displayName: "o1-pro",
             supportsTools: false
         },
         {
@@ -88,24 +48,14 @@ export function getOpenaiModels(): ModelDefinition[] {
             supportsTools: true
         },
         {
-            id: "gpt-4o-2024-08-06",
-            displayName: "GPT 4o-2024-08-06",
+            id: "gpt-4o-search-preview",
+            displayName: "GPT 4o-search-preview",
             supportsTools: true
         },
         {
-            id: "gpt-4o-mini-2024-07-18",
-            displayName: "GPT 4o-mini-2024-07-18",
+            id: "gpt-4o-mini-search-preview",
+            displayName: "GPT 4o-mini-search-preview",
             supportsTools: true
         },
-        {
-            id: "o1-mini",
-            displayName: "o1-mini",
-            supportsTools: false
-        },
-        {
-            id: "o1-mini-2024-09-12",
-            displayName: "o1-mini-2024-09-12",
-            supportsTools: false
-        }
     ];
 }
